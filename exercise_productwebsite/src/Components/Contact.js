82e944e4fba99603c28e8f485f3793754acd663b
sowmyav3 <sowmya26sowmya@gmail.com>
2023-12-28 19:45:41 +0530

    [Contact]

--- exercise_productwebsite/src/Components/Contact.js
+++ exercise_productwebsite/src/Components/Contact.js
@@ -5,7 +5,10 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import LanguageIcon from '@mui/icons-material/Language';
 function Contact(props) {
     const [disabled, setdisabled] = useState("true");
     const [name, setName] = useState('');
@@ -130,21 +133,21 @@ function Contact(props) {
                         backgroundColor: "#5FB2F0",
                     }}>
                         <span className='contact-content1'>Contact us</span>
-                        <div className='mt-4'>
+                        <div className='mt-4'style={{marginLeft:"1rem"}}>
                              <div className='d-flex'>
-                                 <i class="bi bi-geo-alt-fill"></i>
+                                 <LocationOnIcon fontSize='small' ></LocationOnIcon>
                                  <p >Address: hyderabad</p>
                              </div>
                              <div className='d-flex'>
-                                 <i class="bi bi-telephone-fill"></i>
+                                 <LocalPhoneIcon fontSize='small'/>
                                  <p>Contact :- +91 8988276599</p>
                              </div>
                              <div className='d-flex'>
-                                 <i class="bi bi-envelope-fill"></i>
+                                 <EmailIcon fontSize='small'/>
                                  <p>Email:- Contact@gmail.com</p>
                              </div>
                              <div className='d-flex'>
-                                 <i class="bi bi-browser-chrome"></i>
+                                 <LanguageIcon fontSize='small'/>
                              <p>Website: www.contact.com</p>
                              </div>
 
@@ -154,76 +157,6 @@ function Contact(props) {
 
             </div>
         </div>
-        // <div className="container">
-        //     <div className="row">
-        //         <div className='col-md-7'>
-        //             <h1>Get in touch</h1>
-        //             <form className='needs-validation'>
-        //                 <div className='form-group was-validated mb-3'>
-        //                     <label htmlFor='formGroupExampleInput' className='form-label'>
-        //                         Name
-        //                     </label>
-        //                     <input type="text" className='form-control' id="formGroupExampleInput" onChange={namehandleChange} value={name} required></input>
-        //                     <div className='invalid-feedback'>
-        //                         Please enter your name
-        //                     </div>
-        //                 </div>
-        //                 <div className='form-group was-validated mb-3'>
-        //                     <label htmlFor='formGroupExampleInput2' className='form-label'>
-        //                         Email Address
-        //                     </label>
-        //                     <input type="email" className='form-control' id="formGroupExampleInput2" onChange={emailhandleChange} value={email} required></input>
-        //                     <div className='invalid-feedback'>
-        //                         Please enter your email
-        //                     </div>
-        //                 </div>
-        //                 <div className='form-group was-validated mb-3' >
-        //                     <label htmlFor='formGroupExampleInput2' className='form-label'>
-        //                         Contact Number
-        //                     </label>
-        //                     <input type="text" className='form-control' id="formGroupExampleInput2" onChange={NumberhandleChange} value={num} required></input>
-        //                     <div className='invalid-feedback'>
-        //                         Please enter your Phone Number
-        //                     </div>
-        //                 </div>
-        //                 <div className='form-group was-validated mb-3' >
-        //                     <label htmlFor='formGroupExampleControlTextarea1' className='form-label'>
-        //                         Message
-        //                     </label>
-        //                     <input className='form-control' id="exampleFormControlTextarea1" rows={3} onChange={messagehandleChange} value={message} required></input>
-        //                     <div className='invalid-feedback'>
-        //                         Please enter your Message
-        //                     </div>
-        //                 </div>
-        //                 <button classname="btn btn-primary" onClick={Contacthandler}>Submit</button>
-        //             </form>
-
-        //         </div>
-        //         <div className='col-md-5'>
-        //                 <h2>Contact Us</h2>
-        //                 <div className='mt-4'>
-        //                     <div className='d-flex'>
-        //                         <i class="bi bi-geo-alt-fill"></i>
-        //                         <p >Address: hyderabad</p>
-        //                     </div>
-        //                     <div className='d-flex'>
-        //                         <i class="bi bi-telephone-fill"></i>
-        //                         <p>Contact :- +91 8988276599</p>
-        //                     </div>
-        //                     <div className='d-flex'>
-        //                         <i class="bi bi-envelope-fill"></i>
-        //                         <p>Email:- Contact@gmail.com</p>
-        //                     </div>
-        //                     <div className='d-flex'>
-        //                         <i class="bi bi-browser-chrome"></i>
-        //                     <p>Website: www.contact.com</p>
-        //                     </div> 
-
-        //                 </div>
-        //             </div>
-        //     </div>
-
-        // </div>
     );
 }
 
