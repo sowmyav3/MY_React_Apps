acb40b04b09459684a56c5b63dc7f3c40d86584f
sowmyav3 <sowmya26sowmya@gmail.com>
2024-09-17 18:49:53 +0530

    mernstack_course

---
+++ mern-project/frontend/build/precache-manifest.ce2a396ed15434d152f7052262f5bce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2b91102b607d05c109dabc99bca9a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "150d87f2cd843d5801db",
+    "url": "/static/css/main.bcb423a6.chunk.css"
+  },
+  {
+    "revision": "e3e711ed276aa55d9f7e",
+    "url": "/static/js/2.ce676991.chunk.js"
+  },
+  {
+    "revision": "150d87f2cd843d5801db",
+    "url": "/static/js/main.5a564382.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
